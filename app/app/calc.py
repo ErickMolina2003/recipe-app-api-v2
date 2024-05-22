cf84de9306b33aeb714cbfad18f509ecedf25b9a
eralejo2003 <eralejo2003@gmail.com>
2024-05-21 19:06:55 -0600

    test driven development

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator function
+"""
+
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x+y
+
+
+def substract(x, y):
+    """Substract y and x and return result"""
+    return y-x
