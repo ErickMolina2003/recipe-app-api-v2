dd7c0ca9827aca706d6f69540e82b8425f4312c8
eralejo2003 <eralejo2003@gmail.com>
2024-05-27 20:22:33 -0600

    creating the url to enable users on our app, creating url on user to enable create endpoint, create the create user view to attach the user serializer

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -62,5 +62,5 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-        user = get_user_model().objects.get(email=payload['email']).exists()
+        user = get_user_model().objects.filter(email=payload['email']).exists()
         self.assertFalse(user)
