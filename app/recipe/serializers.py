2f45813d203fa4fa1ab938f9da017a51b08124ef
eralejo2003 <eralejo2003@gmail.com>
2024-05-29 19:57:17 -0600

    Implementing a serializer for the recipe detail API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,9 +6,16 @@ from core.models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    """Serializer for the user object"""
+    """Serializer for the recipe object"""
 
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
