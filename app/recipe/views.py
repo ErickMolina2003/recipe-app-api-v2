b2271c85bc7bea67715f662f00c2dedb8107b10f
eralejo2003 <eralejo2003@gmail.com>
2024-05-30 22:05:59 -0600

    edditing the viewset of tags to enable patch and put actions on tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -37,7 +37,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+
+class TagViewSet(mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """View for manage tag APIs"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
