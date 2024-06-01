84ad1c8af7a5e3a8e46276118c9d6cf841d910b9
eralejo2003 <eralejo2003@gmail.com>
2024-05-31 20:25:54 -0600

    fixing linting

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -23,7 +23,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags',]
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags', ]
         read_only_fields = ['id']
 
     def _get_or_create_tags(self, tags, recipe):
