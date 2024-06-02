f1c25b1ab10088f53dfb2931f028fa6e9750e3e1
eralejo2003 <eralejo2003@gmail.com>
2024-06-01 19:26:17 -0600

    writing unitest for update a ingredient

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -12,6 +12,11 @@ from recipe.serializers import IngredientSerializer
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 
+def detail_url(ingredient_id):
+    """create and return ignredient detail url"""
+    return reverse('recipe:ingredient-detail', args=[ingredient_id])
+
+
 def create_user(email='user@example.com', password='testpasas123'):
     """Create and return a new user"""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -64,3 +69,17 @@ class PrivateIngredientsAPITests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
         self.assertEqual(res.data[0]['id'], ingredient.id)
+
+    def test_update_ingredient(self):
+        """Test updating an ingredient"""
+        ingredient = Ingredient.objects.create(
+            user=self.user, name='Ingredient 1')
+
+        payload = {'name': 'Ingredient updated'}
+
+        url = detail_url(ingredient.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        ingredient.refresh_from_db()
+        self.assertEqual(ingredient.name, payload['name'])
